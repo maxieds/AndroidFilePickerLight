e70c224519b045f10ac81559130960e55fe37cfc
Dr. Maxie Dion Schmidt <maxieds@Drs-MacBook-Pro.local>
2026-03-05 05:41:47 -0600

    Release v1.3.6-beta : Attempts to patch jitpack.io library references

--- AndroidFilePickerLightLibrary/build/generated/source/buildConfig/debug/com/maxieds/androidfilepickerlightlibrary/BuildConfig.java
+++ AndroidFilePickerLightLibrary/build/generated/source/buildConfig/debug/com/maxieds/androidfilepickerlightlibrary/BuildConfig.java
@@ -8,13 +8,13 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.maxieds.androidfilepickerlightlibrary";
   public static final String BUILD_TYPE = "debug";
   // Field from build type: debug
-  public static final String BUILD_TIMESTAMP = "2026-03-05-030431";
+  public static final String BUILD_TIMESTAMP = "2026-03-05-052604";
   // Field from build type: debug
   public static final String GIT_COMMIT_DATE = "";
   // Field from build type: debug
   public static final String GIT_COMMIT_HASH = "";
   // Field from build type: debug
-  public static final long VERSION_CODE = 27L;
+  public static final long VERSION_CODE = 28L;
   // Field from build type: debug
-  public static final String VERSION_NAME = "1.3.4-beta";
+  public static final String VERSION_NAME = "1.3.5-beta";
 }
